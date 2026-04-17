3b95c941a330f146e7bf96f11977f5f6b4bdfe52
fuatkermn <106134912+fuatkermn@users.noreply.github.com>
2026-04-17 03:17:08 +0300

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from .database import Database
 from .tool_manager import ToolManager
-from .helpers import SystemInfo, CommandExecutor, ProgressTracker
+from .helpers import SystemInfo, CommandExecutor, ProgressTracker, ToolInstaller
 from .matrix_bg import MatrixBackground
 from .report_generator import ReportGenerator
 from .ai_module import AIModule
 from .voice_module import VoiceModule
 
-__all__ = ['Database', 'ToolManager', 'SystemInfo', 'CommandExecutor', 'ProgressTracker', 'MatrixBackground', 'ReportGenerator', 'AIModule', 'VoiceModule']
+__all__ = [
+    'Database',
+    'ToolManager',
+    'SystemInfo',
+    'CommandExecutor',
+    'ProgressTracker',
+    'ToolInstaller',
+    'MatrixBackground',
+    'ReportGenerator',
+    'AIModule',
+    'VoiceModule'
+]
